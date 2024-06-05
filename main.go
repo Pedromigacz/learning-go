d4968b6634ae34790a93ce3782356c823ddcd128
Pedro H G Migacz <pedromigacz@gmail.com>
2024-06-05 09:48:27 -0300

    learning packages

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	strings.Split("Pedro", "")              // It does't print returned values
+	fmt.Println(strings.Split("Pedro", "")) // Prints theresult of the function
+
 }
