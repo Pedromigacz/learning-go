f4c664b0c642fc6f2319805ef706f8c8319325f9
Pedro H G Migacz <pedromigacz@gmail.com>
2024-06-05 14:07:45 -0300

    add variables syntax

---
+++ lib/types_and_variables.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// fmt.Printf("Type: %T - Value: %v\n", true, true)         // bool
+	// fmt.Printf("Type: %T - Value: %v\n", "Pedro", "Pedro")   // string
+	// fmt.Printf("Type: %T - Value: %v\n", 1, 1)               // int
+	// fmt.Printf("Type: %T - Value: %v\n", 1.233, 1.233)       // float64
+
+	// var name string = "Pedro H" // equivalent to next line
+	// name := "Pedro H" // creates a variable, infers its type and add "Pedro H" as a value
+
+	// fmt.Printf("Type: %T - Value: %v\n", name, name)
+
+	// const age = 5
+	// fmt.Println(age)
+
+}
